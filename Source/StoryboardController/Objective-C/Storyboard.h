d4e9f34c605f3647a9bfc8ec0412dbfdec9c8f8a
Michael Loo <michael.loo@outware.com.au>
2017-07-03 11:39:13 +1000

    Implement `Storyboard` subclass of `UIStoryboard`
    Which role is to monitor view controllers instantiated through the superclass and call a method to inject the dependencies.

---
+++ Source/StoryboardController/Objective-C/Storyboard.h
@@ -0,0 +1,54 @@
+//  Copyright © 2015 Outware Mobile. All rights reserved.
+
+@import UIKit;
+
+/**
+ * When instantiated with a block `inject`, calls that block with the
+ * instantiated view controller whenever a view controller is instantiated from
+ * the receiving storyboard.
+ */
+@interface Storyboard : UIStoryboard
+
+/**
+ * Initialise an instance of this class with a storyboard name.
+ *
+ * @param name Storyboard name
+ *
+ * @return instance of class
+ */
+- (nonnull instancetype)initWithName:(nonnull NSString *)name;
+
+/**
+ * Initialise an instance of this class with a storyboard name and a function to call upon the view controller
+ * in order to inject dependencies.
+ *
+ * @param name    Storyboard name
+ * @param inject  A function which is invoked on an iniatlised view controller to inject dependencies
+ *
+ * @return instance of class
+ */
+- (nonnull instancetype)initWithName:(nonnull NSString *)name inject:(nonnull void (^)(UIViewController * _Nonnull))inject;
+
+/**
+ * Initialise an instance of this class with a storyboard name and the bundle containing the storyboard.
+ *
+ * @param name    Storyboard name
+ * @param bundle  Bundle containing the storyboard
+ *
+ * @return instance of class
+ */
+- (nonnull instancetype)initWithName:(nonnull NSString *)name bundle:(nonnull NSBundle *)bundle;
+
+/**
+ * Initialise an instance of this class with a storyboard name, a function to call upon the view controller
+ * in order to inject dependencies and the bundle containing the storyboard.
+ *
+ * @param name    Storyboard name
+ * @param bundle  Bundle containing the storyboard
+ * @param inject  A function which is invoked on an iniatlised view controller to inject dependencies
+ *
+ * @return instance of class
+ */
+- (nonnull instancetype)initWithName:(nonnull NSString *)name bundle:(nonnull NSBundle *)bundle inject:(nonnull void (^)(UIViewController * _Nonnull))inject;
+
+@end
